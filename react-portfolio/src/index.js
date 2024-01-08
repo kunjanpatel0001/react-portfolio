bef450aac8b72f61199ded00c16723c7a37dc69e
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-01-08 16:23:21 +0530

    Side Bar Done
    Left side bar completed

--- react-portfolio/src/index.js
+++ react-portfolio/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* wrapping the browser router around the app for the ability of using the routes inside of the application */}
+    <BrowserRouter> 
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
